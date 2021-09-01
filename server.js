d37415de8a4c654c61fbdeed8281e114de7aa260
Willian Mesquita <the.willian.mesquita@gmail.com>
2021-09-01 19:57:18 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+
+// Port may vary depending on the network
+// 8080, 9000, 4280
 const port = process.env.PORT || 4000;
 
 server.use(middlewares);
